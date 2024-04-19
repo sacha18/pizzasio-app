1013cbc02771b4cb9e8b2b03b8b52db0a558ea56
sacha18 <103282007+sacha18@users.noreply.github.com>
2024-04-19 12:24:18 +0200

    fix: corriger class

--- app/src/main/java/com/example/pizzasio/data/model/commande/CommandeModel.kt
+++ app/src/main/java/com/example/pizzasio/data/model/commande/CommandeModel.kt
@@ -1,6 +1,6 @@
 package com.example.pizzasio.data.model.commande
 
-data class CommandeModel(id: String, date: String, price: String) (
+data class CommandeModel(
     val id: String,
     val date: String,
     val price: String,
